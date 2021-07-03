3ee8b7ea8798e673fea0dab124e55f9e6ecf96ea
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-02 20:49:53 -0400

    random4

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,41 @@
-import ReactDOM from 'react-dom'
-const Button = () => {
-    return (
-        <>
-            <button onClick={() => { alert("Cringe"); console.log("didsmth");}} className='btn'>Add</button>
-        </>
-    )
-}
+// import React, { useState } from 'react';
+// const Button = () => {
+//     return (
+//         <>
+//             <button onClick={() => { alert("Cringe"); console.log("didsmth");}} className='btn'>Add</button>
+//         </>
+//     )
+// }
 
-export default Button
+// export default Button
+import React, { useState } from 'react';
+
+function Example() {
+  const [showForm, setShowForm] = useState(false);
+
+  const showForm = () => {
+    setShowForm(!showForm);
+  }
+
+  return (
+    <div>
+      <form>
+        <button onClick={showForm}></button>
+      </form>
+
+      {showForm && (
+        <form>
+        <label>
+             Task Name: <br></br>
+            <input type="text" name="name" placeholder="Task Name"/><br></br>
+        </label>
+        <label>
+             Date: <br></br>
+            <input type="text" name="date" placeholder="MM/DD/YYYY"/> <br></br>
+        </label>
+        <input type="submit" value="Submit" />
+    </form>
+      )}
+    </div>
+  )
+}
