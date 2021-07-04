5db1bde23123cd745b1786775485b5a135eb6679
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-04 13:06:15 -0400

    optimized main app function

--- src/App.js
+++ src/App.js
@@ -3,24 +3,62 @@ import Button from './components/Button'
 import Body from './components/Body'
 import Tasks from './components/Tasks'
 
-const App = ()=> {
-  return (
-    <div className='container2'>
-      <div className='container'>
-        <Header/>
-        <Button/>
-        <div className='form-control'>
-         <Body/>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='task'>
-          <Tasks/>
-        </div>
+// const App = ()=> {
+//   return (
+//     <div className='container2'>
+//       <div className='container'>
+//         <Header/>
+//         <Button/>
+//         <div className='form-control'>
+//          <Body/>
+//         </div>
+//       </div>
+//       <div className='container'>
+//         <div className='task'>
+//           <Tasks/>
+//         </div>
         
-      </div>
-    </div>
-  );
+//       </div>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+function App() {
+  // const [state, setState] = useState('start')
+
+  // triggerAddTripState = () => {
+  //   setState('add-trip')
+  // }
+
+  // return (
+  //   <div>
+  //     {state === 'start' && (
+  //       <AddTripButton addTrip={this.triggerAddTripState} />
+  //     )}
+
+  //     {state === 'add-trip' && <AnotherComponent />}
+  //   </div>
+  // )
+  return (
+        <div className='container2'>
+          <div className='container'>
+            <Header/>
+            <Button/>
+            <div className='form-control'>
+             <Body/>
+            </div>
+          </div>
+          <div className='container'>
+            <div className='task'>
+              <Tasks/>
+            </div>
+            
+          </div>
+        </div>
+      );
 }
 
 export default App;
