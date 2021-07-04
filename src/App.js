a5247df34473dfa137f861c386553df5f3906069
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-04 17:57:20 -0400

    dynamic forms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Button from './components/Button'
 import Body from './components/Body'
 import Tasks from './components/Tasks'
+import React, { useState } from 'react';
 
 // const App = ()=> {
 //   return (
@@ -26,39 +27,55 @@ import Tasks from './components/Tasks'
 
 // export default App;
 
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { isEmptyState: true }
+  }
+
+  triggerAddTaskState = () => {
+    this.setState({
+      ...this.state,
+      isEmptyState: false,
+      isAddTripState: true
+    })
+  }
+}
+
+
 function App() {
-  // const [state, setState] = useState('start')
+  const [state, setState] = useState('start')
 
-  // triggerAddTripState = () => {
-  //   setState('add-trip')
-  // }
+  triggerAddTaskState = () => {
+    setState('add-Task')
+  }
 
-  // return (
-  //   <div>
-  //     {state === 'start' && (
-  //       <AddTripButton addTrip={this.triggerAddTripState} />
-  //     )}
-
-  //     {state === 'add-trip' && <AnotherComponent />}
-  //   </div>
-  // )
   return (
-        <div className='container2'>
-          <div className='container'>
-            <Header/>
-            <Button/>
-            <div className='form-control'>
-             <Body/>
-            </div>
-          </div>
-          <div className='container'>
-            <div className='task'>
-              <Tasks/>
-            </div>
+    <div>
+      {state === 'start' && (
+        <Button addTask={this.triggerAddTaskState} />
+      )}
+
+      {state === 'add-Task' && <Body />}
+    </div>
+  )
+  // return (
+  //       <div className='container2'>
+  //         <div className='container'>
+  //           <Header/>
+  //           <Button/>
+  //           <div className='form-control'>
+  //            <Body/>
+  //           </div>
+  //         </div>
+  //         <div className='container'>
+  //           <div className='task'>
+  //             <Tasks/>
+  //           </div>
             
-          </div>
-        </div>
-      );
+  //         </div>
+  //       </div>
+  //     );
 }
 
 export default App;
