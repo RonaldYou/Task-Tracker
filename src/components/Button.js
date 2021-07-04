6cb9dd07d1a0af8c46d51b86e822e5e3c865da7d
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-04 12:46:46 -0400

    addTask form

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,41 +1,10 @@
-// import React, { useState } from 'react';
-// const Button = () => {
-//     return (
-//         <>
-//             <button onClick={() => { alert("Cringe"); console.log("didsmth");}} className='btn'>Add</button>
-//         </>
-//     )
-// }
-
-// export default Button
-import React, { useState } from 'react';
-
-function Example() {
-  const [showForm, setShowForm] = useState(false);
-
-  const showForm = () => {
-    setShowForm(!showForm);
-  }
-
-  return (
-    <div>
-      <form>
-        <button onClick={showForm}></button>
-      </form>
-
-      {showForm && (
-        <form>
-        <label>
-             Task Name: <br></br>
-            <input type="text" name="name" placeholder="Task Name"/><br></br>
-        </label>
-        <label>
-             Date: <br></br>
-            <input type="text" name="date" placeholder="MM/DD/YYYY"/> <br></br>
-        </label>
-        <input type="submit" value="Submit" />
-    </form>
-      )}
-    </div>
-  )
+import React from 'react';
+const Button = (props) => {
+    return (
+        <>
+            <button onClick={props.addTask} className='btn'>Add</button>
+        </>
+    )
 }
+//() => { alert("Cringe"); console.log("didsmth");}
+export default Button
