ebd2fffd596f7efe2e1fe6d6857785f77f70308d
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-06 19:12:25 -0400

    dynamic forms 3

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,66 +20,68 @@
 
 // export default Body
 
-// import React from "react"
-// class Body extends React.Component {
-//   state = {
-//     cats: [{name:"", age:""}],
-//   }
-//   render() {
-//     let {cats} = this.state
-//     return (
-//       <form>
-//         <label htmlFor="name">Owner</label> 
-//         <input type="text" name="owner" id="owner" />
-//         <label htmlFor="description">Description</label> 
-//         <input type="text" name="description" id="description" />
-//         <button>Add new cat</button>
-//         {
-//           cats.map((val, idx)=> {
-//             let catId = `cat-${idx}`, ageId = `age-${idx}`
-//             return (
-//               <div key={idx}>
-//                 <label htmlFor={catId}>{`Cat #${idx + 1}`}</label>
-//                 <input
-//                   type="text"
-//                   name={catId}
-//                   data-id={idx}
-//                   id={catId}
-//                   className="name"
-//                 />
-//                 <label htmlFor={ageId}>Age</label>
-//                 <input
-//                   type="text"
-//                   name={ageId}
-//                   data-id={idx}
-//                   id={ageId}
-//                   className="age"
-//                 />
-//               </div>
-//             )
-//           })
-//         }
-//         <input type="submit" value="Submit" /> 
-//       </form>
-//     )
-//   }
-// }
-// export default Body
-
 import React from "react"
 class Body extends React.Component {
-  
-  render() {
+  state = {
+    task: [{name:"", date:""}],
+    owner: "",
+    description: ""
+  }
+handleChange = (e) => {
+    if (["name", "date"].includes(e.target.className) ) {
+      let task = [...this.state.task]
+      task[e.target.dataset.id][e.target.className] = e.target.value.toUpperCase()
+      this.setState({ task }, () => console.log(this.state.task))
+    } else {
+      this.setState({ [e.target.name]: e.target.value.toUpperCase() })
+    }
+  }
+addCat = (e) => {
+    this.setState((prevState) => ({
+      task: [...prevState.task, {name:"", date:""}],
+    }));
+  }
+handleSubmit = (e) => { e.preventDefault() }
+render() {
+    let {owner, description, task} = this.state
     return (
-      <form>
-        <label htmlFor="owner">Owner</label> 
-        <input type="text" name="owner" id="owner" />
+      <form onSubmit={this.handleSubmit} onChange={this.handleChange} >
+        <label htmlFor="name">Owner</label> 
+        <input type="text" name="owner" id="owner" value={owner} />
         <label htmlFor="description">Description</label> 
-        <input type="text" name="description" id="description" />
-        <button>Add new cat</button>
+        <input type="text" name="description" id="description" value={description} />
+        <button onClick={this.addCat}>Add new cat</button>
+        {
+          task.map((val, idx)=> {
+            let catId = `cat-${idx}`, dateId = `date-${idx}`
+            return (
+              <div key={idx}>
+                <label htmlFor={catId}>{`Cat #${idx + 1}`}</label>
+                <input
+                  type="text"
+                  name={catId}
+                  data-id={idx}
+                  id={catId}
+                  value={task[idx].name} 
+                  className="name"
+                />
+                <label htmlFor={dateId}>Age</label>
+                <input
+                  type="text"
+                  name={dateId}
+                  data-id={idx}
+                  id={dateId}
+                  value={task[idx].date} 
+                  className="date"
+                />
+              </div>
+            )
+          })
+        }
         <input type="submit" value="Submit" /> 
       </form>
     )
   }
 }
 export default Body
+
