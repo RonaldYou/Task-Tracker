0e4699a8531f11c8eab60c0fbcf26c723423c6c6
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-06 19:02:18 -0400

    dynamic forms 2

--- src/App.js
+++ src/App.js
@@ -4,78 +4,25 @@ import Body from './components/Body'
 import Tasks from './components/Tasks'
 import React, { useState } from 'react';
 
-// const App = ()=> {
-//   return (
-//     <div className='container2'>
-//       <div className='container'>
-//         <Header/>
-//         <Button/>
-//         <div className='form-control'>
-//          <Body/>
-//         </div>
-//       </div>
-//       <div className='container'>
-//         <div className='task'>
-//           <Tasks/>
-//         </div>
-        
-//       </div>
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { isEmptyState: true }
-  }
-
-  triggerAddTaskState = () => {
-    this.setState({
-      ...this.state,
-      isEmptyState: false,
-      isAddTripState: true
-    })
-  }
-}
-
-
-function App() {
-  const [state, setState] = useState('start')
-
-  triggerAddTaskState = () => {
-    setState('add-Task')
-  }
-
+const App = ()=> {
   return (
-    <div>
-      {state === 'start' && (
-        <Button addTask={this.triggerAddTaskState} />
-      )}
-
-      {state === 'add-Task' && <Body />}
+    <div className='container2'>
+      <div className='container'>
+        <Header/>
+        <Button/>
+        <div className='form-control'>
+         <Body/>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='task'>
+          <Tasks/>
+        </div>
+        
+      </div>
     </div>
-  )
-  // return (
-  //       <div className='container2'>
-  //         <div className='container'>
-  //           <Header/>
-  //           <Button/>
-  //           <div className='form-control'>
-  //            <Body/>
-  //           </div>
-  //         </div>
-  //         <div className='container'>
-  //           <div className='task'>
-  //             <Tasks/>
-  //           </div>
-            
-  //         </div>
-  //       </div>
-  //     );
+  );
 }
+
 
 export default App;
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,85 @@
-const Body = () => {
-    return (
-        <div className='form-control'>
-         <body>
-              <form>
-                 <label>
-                      Task Name: <br></br>
-                     <input type="text" name="name" placeholder="Task Name"/><br></br>
-                 </label>
-                 <label>
-                      Date: <br></br>
-                     <input type="text" name="date" placeholder="MM/DD/YYYY"/> <br></br>
-                 </label>
-                 <input type="submit" value="Submit" />
-             </form>
-         </body>
-        </div>
-    )
-}
+// const Body = () => {
+//     return (
+//         <div className='form-control'>
+//          <body>
+//               <form>
+//                  <label>
+//                       Task Name: <br></br>
+//                      <input type="text" name="name" placeholder="Task Name"/><br></br>
+//                  </label>
+//                  <label>
+//                       Date: <br></br>
+//                      <input type="text" name="date" placeholder="MM/DD/YYYY"/> <br></br>
+//                  </label>
+//                  <input type="submit" value="Submit" />
+//              </form>
+//          </body>
+//         </div>
+//     )
+// }
 
+// export default Body
+
+// import React from "react"
+// class Body extends React.Component {
+//   state = {
+//     cats: [{name:"", age:""}],
+//   }
+//   render() {
+//     let {cats} = this.state
+//     return (
+//       <form>
+//         <label htmlFor="name">Owner</label> 
+//         <input type="text" name="owner" id="owner" />
+//         <label htmlFor="description">Description</label> 
+//         <input type="text" name="description" id="description" />
+//         <button>Add new cat</button>
+//         {
+//           cats.map((val, idx)=> {
+//             let catId = `cat-${idx}`, ageId = `age-${idx}`
+//             return (
+//               <div key={idx}>
+//                 <label htmlFor={catId}>{`Cat #${idx + 1}`}</label>
+//                 <input
+//                   type="text"
+//                   name={catId}
+//                   data-id={idx}
+//                   id={catId}
+//                   className="name"
+//                 />
+//                 <label htmlFor={ageId}>Age</label>
+//                 <input
+//                   type="text"
+//                   name={ageId}
+//                   data-id={idx}
+//                   id={ageId}
+//                   className="age"
+//                 />
+//               </div>
+//             )
+//           })
+//         }
+//         <input type="submit" value="Submit" /> 
+//       </form>
+//     )
+//   }
+// }
+// export default Body
+
+import React from "react"
+class Body extends React.Component {
+  
+  render() {
+    return (
+      <form>
+        <label htmlFor="owner">Owner</label> 
+        <input type="text" name="owner" id="owner" />
+        <label htmlFor="description">Description</label> 
+        <input type="text" name="description" id="description" />
+        <button>Add new cat</button>
+        <input type="submit" value="Submit" /> 
+      </form>
+    )
+  }
+}
 export default Body
