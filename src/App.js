02c263b73822fb2fa076ef2c8d1403a90d1b5cad
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-14 21:47:13 -0400

    react-router-dom installation

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import Button from './components/Button'
 import Body from './components/Body'
 import Tasks from './components/Tasks'
 import pdf from "./pdf/pdf";
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
+import React, { Component } from "react";
+import { Router, Switch, Route } from "react-router-dom";
 
 const App = ()=> {
   return (
-    <div className='container2'>
-      <div className='container'>
-        <Header/>
-        <Button/>
-        <div className='form-control'>
-         <Body/>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='task'>
-          <Tasks/>
-        </div>
+    // <div className='container2'>
+    //   <div className='container'>
+    //     <Header/>
+    //     <Button/>
+    //     <div className='form-control'>
+    //      <Body/>
+    //     </div>
+    //   </div>
+    //   <div className='container'>
+    //     <div className='task'>
+    //       <Tasks/>
+    //     </div>
         
-      </div>
-    </div>
+    //   </div>
+    // </div>
+    <Router history={button}>
+                <Switch>
+                    <Route path="/pdf" exact component={pdf} />
+                </Switch>
+            </Router>
   );
 }
 
