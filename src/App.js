87265144e3507cfe8549a113228d7263b32b6103
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2021-07-15 21:49:08 -0400

    fix to previous version

--- src/App.js
+++ src/App.js
@@ -2,33 +2,25 @@ import Header from './components/Header'
 import Button from './components/Button'
 import Body from './components/Body'
 import Tasks from './components/Tasks'
-import pdf from "./pdf/pdf";
-//import React, { useState } from 'react';
-import React, { Component } from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import React, { useState } from 'react';
 
 const App = ()=> {
   return (
-    // <div className='container2'>
-    //   <div className='container'>
-    //     <Header/>
-    //     <Button/>
-    //     <div className='form-control'>
-    //      <Body/>
-    //     </div>
-    //   </div>
-    //   <div className='container'>
-    //     <div className='task'>
-    //       <Tasks/>
-    //     </div>
+    <div className='container2'>
+      <div className='container'>
+        <Header/>
+        <Button/>
+        <div className='form-control'>
+         <Body/>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='task'>
+          <Tasks/>
+        </div>
         
-    //   </div>
-    // </div>
-    <Router history={button}>
-                <Switch>
-                    <Route path="/pdf" exact component={pdf} />
-                </Switch>
-            </Router>
+      </div>
+    </div>
   );
 }
 
